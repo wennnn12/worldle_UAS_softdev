5ccba4deb1d0982cbdced3a3aa071b38ba8effe7
George535220173 <george.535220173@stu.untar.ac.id>
2024-06-08 17:36:37 +0700

    Session gamemode between account

--- lib/setting.dart
+++ lib/setting.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'gameEasy.dart';
 import 'gameMedium.dart';
 import 'gameHard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class SettingPage extends StatefulWidget {
   @override
@@ -11,6 +14,44 @@ class SettingPage extends StatefulWidget {
 class _SettingPageState extends State<SettingPage> {
   int _difficultyLevel = 0; // 0: Easy, 1: Medium, 2: Hard
   bool _isDarkMode = false;
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserSettings();
+  }
+
+Future<void> _loadUserSettings() async {
+  user = FirebaseAuth.instance.currentUser;
+  if (user != null) {
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user!.uid).get();
+    setState(() {
+      _difficultyLevel = 0; // Default value
+      _isDarkMode = false; // Default value
+      try {
+        _difficultyLevel = userDoc.get('difficultyLevel');
+      } catch (e) {
+        // Field doesn't exist, keep default value
+      }
+      try {
+        _isDarkMode = userDoc.get('isDarkMode');
+      } catch (e) {
+        // Field doesn't exist, keep default value
+      }
+    });
+  }
+}
+
+
+  Future<void> _saveUserSettings() async {
+    if (user != null) {
+      await FirebaseFirestore.instance.collection('users').doc(user!.uid).set({
+        'difficultyLevel': _difficultyLevel,
+        'isDarkMode': _isDarkMode,
+      }, SetOptions(merge: true));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +95,7 @@ class _SettingPageState extends State<SettingPage> {
                     onChanged: (double value) {
                       setState(() {
                         _difficultyLevel = value.round();
+                        _saveUserSettings(); // Save settings on change
                       });
                     },
                   ),
@@ -68,6 +110,7 @@ class _SettingPageState extends State<SettingPage> {
                     onChanged: (bool value) {
                       setState(() {
                         _isDarkMode = value;
+                        _saveUserSettings(); // Save settings on change
                       });
                     },
                   ),
