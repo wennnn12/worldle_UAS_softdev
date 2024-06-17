eaae6f26aa92ba566d97ecf40adc49587c82fe58
Gawira <149558045+Gawira@users.noreply.github.com>
2024-06-17 20:18:31 +0700

    Admin Half Done

--- lib/admin/admin.dart
+++ lib/admin/admin.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../mainmenu.dart';
-
 import 'manageaccount.dart';
 import 'wordlists.dart';
 
@@ -24,75 +23,133 @@ class AdminPage extends StatelessWidget {
     return WillPopScope(
       onWillPop: () async => false,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('Admin'),
-          centerTitle: true,
-          elevation: 0,
-          automaticallyImplyLeading: false, // Remove the back button
-        ),
+        backgroundColor: Color.fromRGBO(249,254,254, 1),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 20),
               Text(
                 'STATISTICS',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                style: TextStyle(
+                  fontFamily: 'FranklinGothic',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 38,
+                ),
               ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance.collection('users').snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return DisplayBox(title: 'Account Registered', value: 'Loading...');
-                      }
-                      int registeredCount = snapshot.data!.docs.length;
-                      return DisplayBox(title: 'Account Registered', value: registeredCount.toString());
-                    },
-                  ),
-                  StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance.collection('Wordlists').snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return DisplayBox(title: 'Word Lists', value: 'Loading...');
-                      }
-                      int wordListsCount = snapshot.data!.docs.length;
-                      return DisplayBox(title: 'Word Lists', value: wordListsCount.toString());
-                    },
-                  ),
-                  StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance.collection('deleted_accounts').snapshots(),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return DisplayBox(title: 'Deleted Accounts', value: 'Loading...');
-                      }
-                      int deletedCount = snapshot.data!.docs.length;
-                      return DisplayBox(title: 'Deleted Accounts', value: deletedCount.toString());
-                    },
-                  ),
-                ],
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return DisplayBox(title: 'Accounts', value: 'Loading...');
+                        }
+                        int registeredCount = snapshot.data!.docs.length;
+                        return DisplayBox(title: 'Accounts', value: registeredCount.toString());
+                      },
+                    ),
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection('Wordlists').snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return DisplayBox(title: 'Words List', value: 'Loading...');
+                        }
+                        int wordListsCount = snapshot.data!.docs.length;
+                        return DisplayBox(title: 'Words List', value: wordListsCount.toString());
+                      },
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ManageAccountPage()),
-                  );
-                },
-                child: Text('Manage Account'),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection('deleted_accounts').snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return DisplayBox(title: 'Accounts Banned', value: 'Loading...');
+                        }
+                        int deletedCount = snapshot.data!.docs.length;
+                        return DisplayBox(title: 'Accounts Banned', value: deletedCount.toString());
+                      },
+                    ),
+                    StreamBuilder<QuerySnapshot>(
+                      stream: FirebaseFirestore.instance.collection('ProhibitedWords').snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return DisplayBox(title: 'Prohibited Words', value: 'Loading...');
+                        }
+                        int prohibitedWordsCount = snapshot.data!.docs.length;
+                        return DisplayBox(title: 'Prohibited Words', value: prohibitedWordsCount.toString());
+                      },
+                    ),
+                  ],
+                ),
               ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => WordListsPage()),
-                  );
-                },
-                child: Text('Manage Words'),
+              SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => ManageAccountPage()),
+                        );
+                      },
+                      child: Text(
+                        'MANAGE ACCOUNT',
+                        style: TextStyle(
+                          fontFamily: 'FranklinGothic',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 70, 70, 70)
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 164,211,255),
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => WordListsPage()),
+                        );
+                      },
+                      child: Text(
+                        'MANAGE WORDS',
+                        style: TextStyle(
+                          fontFamily: 'FranklinGothic',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 255, 254, 219)
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 70, 70, 70),
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               SizedBox(height: 20),
               ElevatedButton(
@@ -110,7 +167,22 @@ class AdminPage extends StatelessWidget {
                     ),
                   );
                 },
-                child: Text('Logout'),
+                child: Text(
+                  'Logout',
+                  style: TextStyle(
+                    fontFamily: 'FranklinGothic',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Color.fromARGB(255, 70, 70, 70)
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 208, 214, 219),
+                  minimumSize: Size(150, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
               ),
             ],
           ),
@@ -129,16 +201,32 @@ class DisplayBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: MediaQuery.of(context).size.width * 0.4,
       padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        border: Border.all(),
+        color: Color.fromARGB(255, 208, 214, 219),
+        borderRadius: BorderRadius.circular(20),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(title),
+          Text(
+            title,
+            style: TextStyle(
+              fontFamily: 'FranklinGothic',
+              fontSize: 16,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 10),
           Text(
             value,
-            style: TextStyle(fontWeight: FontWeight.bold),
+            style: TextStyle(
+              fontFamily: 'FranklinGothic',
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+            ),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
